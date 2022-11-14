ba7fc1338af41085e6f8c162799a39f05d00d8d4
Alper <alperyorgun2@gmail.com>
2022-11-14 19:25:24 +0300

    problem 14

---
+++ algorithms/src/questions/problem_14_longest_common_prefix.java
@@ -0,0 +1,45 @@
+package questions;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class problem_14_longest_common_prefix {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.longestCommonPrefix(new String[]{"flower","flow","flight"}));
+    }
+    /*
+    Input: strs = ["flower","flow","flight"]
+Output: "fl"
+     */
+    static class Solution {
+        public String longestCommonPrefix(String[] strs) {
+            int count = 0;
+            int arrayIndex = 0;
+            int maxIndex = strs[0].length();
+            for (int i = 0; i < strs.length; i++) {
+                if (maxIndex > strs[i].length()){
+                    maxIndex = strs[i].length() - 1;
+                }
+                if (strs[i].length() == 1){
+                    maxIndex = 1;
+                }if (strs[i].length() == 2){
+                    maxIndex = 2;
+                }
+            }
+            ArrayList<Integer> counts = new ArrayList<Integer>();
+            for (int i = 1; i < strs.length; i++) {
+                for (int j = 0; j < maxIndex; j++) {
+                    if (strs[arrayIndex].charAt(j) == strs[i].charAt(j)){
+                        count+=1;
+                    }
+                }
+                counts.add(count);
+                count = 0;
+            }
+            int arrayCountsIndex = Collections.min(counts);
+           String getChar = strs[0].substring(0,arrayCountsIndex);
+            return getChar;
+        }
+    }
+}
